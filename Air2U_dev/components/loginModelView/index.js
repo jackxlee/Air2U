74de9a7eb164dd249b34d58b79ae9ab8ad60db66
Eric Koay <eric@innovationo2o.com>
2017-01-17 11:33:14 +0000

    Changes made by Eric Koay (eric@innovationo2o.com).

--- Air2U_dev/components/loginModelView/index.js
+++ Air2U_dev/components/loginModelView/index.js
@@ -4,70 +4,71 @@ var apiKey = "o6yuauaw7f5m56jb";
 var el = new Everlive(apiKey);
 
 app.loginModelView = kendo.observable({
-	onShow: function () { },
-	afterShow: function () { },
+    onShow: function() {},
+    afterShow: function() {},
 	submit: function () {
-		if (!this.username) {
-			navigator.notification.alert("Username is required.");
-			return;
-		}
-		if (!this.password) {
-			navigator.notification.alert("Password is required.");
-			return;
-		}
-		el.Users.login(this.username, this.password,
-			function (data) {
-				window.location.href = "#list";
-				app.mobileApp.navigate('components/home/view.html');
-			}, function () {
-				navigator.notification.alert("Unfortunately we could not find your account.");
-			});
-	},
+            if (!this.username) {
+                navigator.notification.alert("Username is required.");
+                return;
+            }
+            if (!this.password) {
+                navigator.notification.alert("Password is required.");
+                return;
+            }
+            el.Users.login(this.username, this.password,
+                function (data) {
+                    window.location.href = "#list";
+
+					app.mobileApp.navigate('components/home/view.html');
+                }, function () {
+                    navigator.notification.alert("Unfortunately we could not find your account.");
+                });
+        },
 });
 app.localization.registerView('loginModelView');
 
 app.registerView = kendo.observable({
 	submit: function () {
-		if (!this.username) {
-			navigator.notification.alert("Username is required.");
-			return;
-		}
-		if (!this.password) {
-			navigator.notification.alert("Password is required.");
-			return;
-		}
-		el.Users.register(this.username, this.password, { Email: this.email },
-			function () {
-				navigator.notification.alert("Your account was successfully created.");
-				window.location.href = "#login";
-			},
-			function () {
-				navigator.notification.alert("Unfortunately we were unable to create your account.");
-			});
-	}
+            if (!this.username) {
+                navigator.notification.alert("Username is required.");
+                return;
+            }
+            if (!this.password) {
+                navigator.notification.alert("Password is required.");
+                return;
+            }
+            el.Users.register(this.username, this.password, { Email: this.email },
+                function () {
+                    navigator.notification.alert("Your account was successfully created.");
+                    window.location.href = "#login";
+                },
+                function () {
+                    navigator.notification.alert("Unfortunately we were unable to create your account.");
+                });
+        }
 });
 app.localization.registerView('registerView');
 
 app.passwordView = kendo.observable({
 	submit: function () {
-		if (!this.email) {
-			navigator.notification.alert("Email address is required.");
-			return;
-		}
-		$.ajax({
-			type: "POST",
-			url: "https://api.everlive.com/v1/" + apiKey + "/Users/resetpassword",
-			contentType: "application/json",
-			data: JSON.stringify({ Email: this.email }),
-			success: function () {
-				navigator.notification.alert("Your password was successfully reset. Please check your email for instructions on choosing a new password.");
-				window.location.href = "#login";
-			},
-			error: function () {
-				navigator.notification.alert("Unfortunately, an error occurred resetting your password.")
-			}
-		});
-	}
+            if (!this.email) {
+                navigator.notification.alert("Email address is required.");
+                return;
+            }
+            $.ajax({
+                type: "POST",
+                url: "https://api.everlive.com/v1/" + apiKey + "/Users/resetpassword",
+                contentType: "application/json",
+                data: JSON.stringify({ Email: this.email }),
+                success: function () {
+                    navigator.notification.alert("Your password was successfully reset. Please check your email for instructions on choosing a new password.");
+                    window.location.href = "#login";
+                },
+                error: function () {
+                    navigator.notification.alert("Unfortunately, an error occurred resetting your password.")
+                }
+            });
+        }
 });
 app.localization.registerView('passwordView');
 // START_CUSTOM_CODE_loginModelView
